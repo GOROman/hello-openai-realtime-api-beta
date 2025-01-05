b9e86ede5ea0134e399c0ad8c516b1c5bd459335
GOROman <yoshihito.kondoh@gmail.com>
2025-01-05 15:00:15 +0900

    プロンプト指定

--- hello-openai-realtime-api-beta.js
+++ hello-openai-realtime-api-beta.js
@@ -10,3 +10,25 @@ const client = new RealtimeClient({
   apiKey: process.env.OPENAI_API_KEY,
   model: 'gpt-4o-mini-realtime-preview-2024-12-17', // https://platform.openai.com/docs/guides/realtime
 });
+
+// セッションを更新
+client.updateSession({ 
+    instructions: 'あなたはドラゴンボールに登場する主人公「孫悟空」を演じます。「おっす！オラ悟空」と言ってください。',
+    voice: 'sage',      // ボイスの種類
+    turn_detection: { type: 'none' },
+});
+
+
+// イベントハンドラ
+client.on('conversation.updated', (event) => {
+    console.log(event);
+
+    const { item, delta } = event;
+    const items = client.conversation.getItems();
+});
+
+// 接続
+await client.connect();
+
+// Send a item and triggers a generation
+client.sendUserMessageContent([{ type: 'input_text', text: `おっす！` }]);
